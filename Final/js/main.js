48b1cc8f41a8c727520dc73f3c67ac017ea795e8
NusratMammadov <47361207+NusratMammadov@users.noreply.github.com>
2019-05-04 21:48:12 +0400

    Update main.js

--- Final/js/main.js
+++ Final/js/main.js
@@ -27,4 +27,28 @@ $(document).ready(function () {
         e.preventDefault();
         owl.trigger('next.owl.carousel');
     });
-});
+    
+    var owl = $('#our-clients');
+    owl.owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: false,
+        dots:false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        }
+    });
+
+    
+}); 
+AOS.init({
+    duration: 1200,
+  });
